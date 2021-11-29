6df154d9832f5c879ef1207026de066fd922ef62
Andrea Giorgi <andrea_giorgi@outlook.it>
2021-11-29 13:37:54 +0100

    [NNU] New module

--- src/neural_network/model_initializer.py
+++ src/neural_network/model_initializer.py
@@ -1,4 +1,9 @@
 def init_training_session():
+    # TODO: Caricare dati di config tramite JSON
+    # TODO: Controllo formattazione JSON per evitare errori
+    # TODO: Definizione parametri di default in caso di mancanza di file JSON
+    # TODO: Definizione funzione che crea un tf.estimator
+    # TODO: Definizione funzione che carica i dati in input a partire dal TFRecord di training e di evaluation
     return
 
 def init_evaluation_session():

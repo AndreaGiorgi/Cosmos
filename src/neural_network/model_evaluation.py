3e44adebef23d8d5ee5f5b4b913ea3622dd452b6
Andrea Giorgi <andrea_giorgi@outlook.it>
2022-02-22 17:18:02 +0100

    new config

--- src/neural_network/model_evaluation.py
+++ src/neural_network/model_evaluation.py
@@ -1,33 +1,12 @@
-from sklearn import neural_network
-from sklearn.metrics import rand_score
 import  tensorflow as tf
 import pandas as pd
 import seaborn as sns
 import numpy as np
 import sklearn
 import datetime
-from scipy.stats import sem
 from matplotlib import pyplot
-from keras import backend as K
 from coordinator.neural_network_coordinator import hybrid_dataset_augmentation_init, start_dataset_postprocessing, hybrid_dataset_formatter_init
 
-
-def recall_m(y_true, y_pred):
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-    recall = true_positives / (possible_positives + K.epsilon())
-    return recall
-
-def precision_m(y_true, y_pred):
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + K.epsilon())
-    return precision
-
-def f1_m(y_true, y_pred):
-    precision = precision_m(y_true, y_pred)
-    recall = recall_m(y_true, y_pred)
-    return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
 def results_visualization(acc_per_fold):
 
@@ -37,7 +16,7 @@ def results_visualization(acc_per_fold):
     ax.set(ylabel='accuracy')
     pyplot.show()
 
-def hybrid_kfold_evaluation(model, cnn_train_dataset, cnn_val_dataset, cnn_test_dataset, snn_train_dataset, snn_val_dataset, snn_test_dataset,config):
+def hybrid_kfold_evaluation(model, cnn_train_dataset, cnn_val_dataset, cnn_test_dataset, snn_train_dataset, snn_val_dataset, snn_test_dataset, config):
 
     cnn_train_dataset = hybrid_dataset_formatter_init('cnn',cnn_train_dataset)
     cnn_val_dataset = hybrid_dataset_formatter_init('cnn', cnn_val_dataset)
@@ -64,15 +43,13 @@ def hybrid_kfold_evaluation(model, cnn_train_dataset, cnn_val_dataset, cnn_test_
 
     model_dir = 'F:\Cosmos\Cosmos\src\model_checkpoint\model_hybrid' + '.h5'
     model.save(model_dir)
-    kfold = sklearn.model_selection.RepeatedKFold(n_splits = 5, n_repeats= 1, random_state = 42)
+    kfold =  sklearn.model_selection.RepeatedKFold(n_splits = 5, n_repeats=2)
 
     for train, test in kfold.split(hybrid_x_cnn, hybrid_y):
         KFold_cnn_train.append(hybrid_x_cnn[train])
         KFold_y.append(hybrid_y[train])
         KFold_cnn_test.append(hybrid_x_cnn[test])
         KFold_y_test.append(hybrid_y[test])
-
-    for train, test in kfold.split(hybrid_x_snn, hybrid_y):
         KFold_snn_train.append(hybrid_x_snn[train])
         KFold_snn_test.append(hybrid_x_snn[test])
 
@@ -80,6 +57,7 @@ def hybrid_kfold_evaluation(model, cnn_train_dataset, cnn_val_dataset, cnn_test_
         fold_model = tf.keras.models.load_model(model_dir)
         _ = fold_model.fit([KFold_cnn_train[i], KFold_snn_train[i]], KFold_y[i], batch_size=config.batch_size, epochs=25, use_multiprocessing=True)
 
+        y_pred = fold_model.predict([KFold_cnn_test[i], KFold_snn_test[i]], batch_size = config.batch_size)
         scores = fold_model.evaluate([KFold_cnn_test[i], KFold_snn_test[i]], KFold_y_test[i], batch_size = config.batch_size, verbose= 1, use_multiprocessing = True)
         acc_per_fold.append(scores[1] * 100)
         loss_per_fold.append(scores[0])
@@ -87,26 +65,9 @@ def hybrid_kfold_evaluation(model, cnn_train_dataset, cnn_val_dataset, cnn_test_
     print('------------------------------------------------------------------------')
     print('Average scores for all folds:')
     print(f'> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)})')
-    print(f'> Loss: {np.mean(loss_per_fold)}')
+    print(f'> Loss: {np.mean(loss_per_fold)} (+- {np.std(loss_per_fold)})')
     print('------------------------------------------------------------------------')
 
-
-def combined_model_evaluation(model, cnn_dataset, cnn_val_dataset, cnn_test_dataset, snn_dataset, snn_val_dataset, snn_test_dataset,config):
-
-    cnn_x_train, cnn_y_train = start_dataset_postprocessing('cnn', cnn_dataset, train = True)
-    cnn_x_test, cnn_y_test = start_dataset_postprocessing('cnn', cnn_test_dataset, train = False)
-    ann_x_train, ann_y_train = start_dataset_postprocessing('snn', snn_dataset, train = True)
-    ann_x_test, ann_y_test = start_dataset_postprocessing('snn', snn_test_dataset, train = False)
-
-    _ = model.fit([cnn_x_train, ann_x_train], cnn_y_train, batch_size=config.batch_size, epochs=10, use_multiprocessing=True)
-    ct = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    model.save('F:\Cosmos\Cosmos\src\model_checkpoint\model_hybrid'+ str(ct) + '.h5')
-    loss, accuracy = model.evaluate([cnn_x_test, ann_x_test], cnn_y_test, verbose=0)
-    print('------------------------------------------------------------------------')
-    print('Combined Cosmos Scores:')
-    print(f'> Accuracy: {accuracy * 100}')
-    print(f'> Loss: {loss}')
-    print('------------------------------------------------------------------------')
 
 def model_kfold_evaluation(model_type, model, dataset, val_dataset, test_dataset, config):
 
@@ -122,7 +83,7 @@ def model_kfold_evaluation(model_type, model, dataset, val_dataset, test_dataset
 
     acc_per_fold = []
     loss_per_fold = []
-    kfold =  sklearn.model_selection.RepeatedKFold(n_splits = 5, n_repeats=5)
+    kfold =  sklearn.model_selection.RepeatedKFold(n_splits = 5, n_repeats= 2)
     for train, test in kfold.split(kf_inputs, kf_targets):
         fold_model = tf.keras.models.load_model(model_dir)
         _ = fold_model.fit(kf_inputs[train], kf_targets[train],
@@ -130,6 +91,7 @@ def model_kfold_evaluation(model_type, model, dataset, val_dataset, test_dataset
               epochs=25,
               use_multiprocessing=True)
 
+        y_pred = fold_model.predict(kf_inputs[test], batch_size = config.batch_size)
         scores = fold_model.evaluate(kf_inputs[test], kf_targets[test], batch_size = 64, verbose=1, use_multiprocessing = True)
         acc_per_fold.append(scores[1] * 100)
         loss_per_fold.append(scores[0])
@@ -137,5 +99,5 @@ def model_kfold_evaluation(model_type, model, dataset, val_dataset, test_dataset
     print('------------------------------------------------------------------------')
     print('Average scores for all folds:')
     print(f'> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)})')
-    print(f'> Loss: {np.mean(loss_per_fold)}')
+    print(f'> Loss: {np.mean(loss_per_fold)} (+- {np.std(loss_per_fold)})')
     print('------------------------------------------------------------------------')
--- src/neural_network/model_initializer.py
+++ src/neural_network/model_initializer.py
@@ -2,10 +2,10 @@
 import tensorflow as tf
 from keras.utils.vis_utils import plot_model
 from keras import Model, Input
-from keras.layers import Dense, BatchNormalization, Dropout, Conv1D, MaxPooling1D, AveragePooling1D, Flatten
+from keras.layers import Dense, BatchNormalization, Conv1D, MaxPooling1D, AveragePooling1D, Flatten
 from keras.initializers import RandomNormal, Constant
 from keras.layers.merge import concatenate
-from neural_network.model_evaluation import combined_model_evaluation, hybrid_kfold_evaluation, model_kfold_evaluation
+from neural_network.model_evaluation import hybrid_kfold_evaluation, model_kfold_evaluation
 
 def _snn_builder(config):
 
@@ -15,11 +15,11 @@ def _snn_builder(config):
     for i in range(0, config.layers_num):
         x = Dense(config.units, activation = config.activation, name = "snn_dense_" + str(i), kernel_initializer=initializer)(x)
         x = tf.keras.layers.AlphaDropout(config.dropout_fc)(x)
-    x = Dense(config.units, activation = config.activation, name = "snn_dense_", kernel_initializer=initializer)(x)
+    x = Dense(config.units, activation = config.activation, name = "snn_dense", kernel_initializer=initializer)(x)
     outputs = Dense(int(config.output_dim), activation=config.output_act, name="predictions")(x)
 
     model = Model(inputs=snn_inputs, outputs=outputs)
-    model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adamax(learning_rate=2e-3, beta_1=0.9, beta_2=0.999, epsilon=1e-07), metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adamax(learning_rate=config.learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-07), metrics=['accuracy'])
     plot_model(model, show_shapes=True, to_file='F:\Cosmos\Cosmos\src\model_checkpoint\cosmos_snn.png')
 
     return model, snn_inputs, x
@@ -38,7 +38,7 @@ def _dcnn_builder(config):
             x = Conv1D(filters=num_filters, kernel_size=int(config.kernel), padding=config.padding, activation=str(config.activation), kernel_initializer=initializer)(x)
         x = AveragePooling1D(pool_size=int(config.pool),  name = "AvgPool_" + str(i), strides=int(config.stride))(x)
         x = tf.keras.layers.SpatialDropout1D(0.25)(x)
-    x = MaxPooling1D(pool_size=int(config.pool),  name = "MaxoPool", strides=int(config.stride))(x)
+    x = MaxPooling1D(pool_size=int(config.pool),  name = "MaxPool", strides=int(config.stride))(x)
     x_flatten = Flatten()(x)
     x = tf.keras.layers.AlphaDropout(config.dropout_fc)(x_flatten)
     for _ in range(config.fc_layers_num):
@@ -46,29 +46,29 @@ def _dcnn_builder(config):
     outputs = Dense(int(config.output_dim), activation=config.output_act, name="predictions")(x)
 
     model = Model(inputs=cnn_inputs, outputs=outputs)
-    model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adagrad(), metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4,beta_1=0.9,beta_2=0.999,epsilon=1e-07, amsgrad=True), metrics=['accuracy'])
     plot_model(model, show_shapes=True, to_file='F:\Cosmos\Cosmos\src\model_checkpoint\cosmos_cnn.png')
 
     return model, cnn_inputs, x_flatten
 
 
-def _combined_snn_builder(snn, dcnn_flatten, snn_inputs, dcnn_inputs, config):
+def _hybrid_builder(snn, dcnn_flatten, snn_inputs, dcnn_inputs, config):
 
     initializer = tf.keras.initializers.LecunNormal()
     merged_model = concatenate([dcnn_flatten, snn])
-    x = Dropout(config.dropout_fc)(merged_model)
-    x = BatchNormalization(momentum=0.95,epsilon=0.005,beta_initializer=RandomNormal(mean=0.0, stddev=0.05),gamma_initializer=Constant(value=0.9))(snn_inputs)
-    for _ in range(3):
-        x = Dense(config.units_hybrid, activation = config.activation, kernel_initializer=initializer)(x)
+    x = tf.keras.layers.AlphaDropout(config.dropout_fc)(merged_model)
+    x = BatchNormalization(momentum=0.95,epsilon=0.005, beta_initializer=RandomNormal(mean=0.0, stddev=0.05),gamma_initializer = Constant(value=0.9))(x)
+    for _ in range(1, config.layers_num):
+        x = Dense(config.units, activation = config.activation, kernel_initializer=initializer)(x)
     outputs = Dense(int(config.output_dim), activation=config.output_act, name="Output")(x)
     model = Model(inputs=[dcnn_inputs, snn_inputs], outputs=outputs)
-    model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.SGD(learning_rate=1e-3, decay=1e-6, momentum=0.9, nesterov=True), metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adagrad(learning_rate=0.001,initial_accumulator_value=0.1,epsilon=1e-07), metrics=['accuracy'])
     plot_model(model, show_shapes=True, to_file='F:\Cosmos\Cosmos\src\model_checkpoint\cosmos_hybrid.png')
 
     return model
 
 
-def _model_builder(config_snn, config_cnn):
+def _model_builder(config_snn, config_cnn, config_hybrid):
 
 
     '''
@@ -120,17 +120,17 @@ def _model_builder(config_snn, config_cnn):
             Ultimo modulo della rete complessa di cosmos. Effettua la fase di classificazione finale.
 
     '''
-    model_hybrid = _combined_snn_builder(snn_last_layer, cnn_last_layer, snn_inputs, cnn_inputs, config_snn)
+    model_hybrid = _hybrid_builder(snn_last_layer, cnn_last_layer, snn_inputs, cnn_inputs, config_hybrid)
 
     return model_snn, model_cnn, model_hybrid
 
 
-def _test_build(local, lc_train_dataset, aux_train_dataset, lc_valid_dataset, aux_valid_dataset, lc_test_dataset, aux_test_dataset, config_snn, config_cnn):
-    model_snn, model_cnn, model_hybrid = _model_builder(config_snn, config_cnn)
+def _test_build(local, lc_train_dataset, aux_train_dataset, lc_valid_dataset, aux_valid_dataset, lc_test_dataset, aux_test_dataset, config_snn, config_cnn, config_hybrid):
+    model_snn, model_cnn, model_hybrid = _model_builder(config_snn, config_cnn, config_hybrid)
 
-    #model_kfold_evaluation('snn', model_snn, aux_train_dataset, aux_valid_dataset, aux_test_dataset, config_snn)
+    model_kfold_evaluation('snn', model_snn, aux_train_dataset, aux_valid_dataset, aux_test_dataset, config_snn)
     #model_kfold_evaluation('cnn', model_cnn, lc_train_dataset, lc_valid_dataset, lc_test_dataset, config_cnn)
-    hybrid_kfold_evaluation(model_hybrid, lc_train_dataset, lc_valid_dataset, lc_test_dataset, aux_train_dataset, aux_valid_dataset, aux_test_dataset, config_cnn)
+    #hybrid_kfold_evaluation(model_hybrid, lc_train_dataset, lc_valid_dataset, lc_test_dataset, aux_train_dataset, aux_valid_dataset, aux_test_dataset, config_hybrid)
 
     return True
 
